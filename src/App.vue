6233259cf94fd5f5db5f77e5e8f4ba5d8edc9a36
RoimerMaestre <roimer.maestre93@gmail.com>
2024-07-24 20:11:10 -0400

    se crea repositorio

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <table>
+      <thead>
+        <tr>
+          <th>Código</th>
+          <th>Nombre</th>
+          <th>Stock</th>
+          <th>Precio</th>
+          <th>Incrementar o decrementar</th>
+       
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="game in games" :key="game.codigo">
+          <td>{{ game.codigo }}</td>
+          <td>{{ game.nombre }}</td>
+          <td>{{ game.stock }}</td>
+          <td>{{ game.precio }}</td>
+          
+          <td>
+            <button @click="decrementStock(game.codigo)">-</button>
+            <button @click="incrementStock(game.codigo)">+</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState, mapActions } from 'vuex';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  computed: {
+    ...mapState({
+      games: (["games"]),
+    }),
+  },
+  methods: {
+    ...mapActions(['incrementStock', 'decrementStock']),
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th, td {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+th {
+  background-color: #f2f2f2;
+}
+
+button {
+  margin: 0 5px;
+  padding: 5px 10px;
 }
 </style>
